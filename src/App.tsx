c19e0abeab615326000fe41bc361f620e776701c
Amadeus <amadeuskang@gmail.com>
2024-03-26 19:24:45 -0400

    Added spinner

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,13 @@ import { Text } from "@chakra-ui/react";
 
 import { Input, InputGroup, InputLeftElement, Stack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import { Spinner } from "@chakra-ui/react";
 
 // import { KeyboardEvent } from "react";
 // import { KeyboardEventHandler } from "react";
 
 const API_URL = "na1.api.riotgames.com";
 // const API_KEY = process.env.REACT_APP_API_KEY;
-
 
 // let iconID = 0;
 
@@ -28,6 +28,7 @@ export default function App() {
   const [summonerData, setSummonerData] = useState<SummonerData | null>(null);
   const [leagueData, setLeagueData] = useState<LeagueData[]>([]);
   const [textInput, setTextInput] = useState("");
+  const [spinner, setSpinner] = useState(false);
 
   const handleButtonClick = () => {
     // get the input field's value
@@ -66,8 +67,11 @@ export default function App() {
     if (textInput.length >= 3 && textInput.length <= 16) {
       const fetchData = async () => {
         try {
+          setSpinner(true);
           const summonerResponse = await fetch(
-            `https://${API_URL}/lol/summoner/v4/summoners/by-name/${textInput}?api_key=${import.meta.env.VITE_API_KEY}`
+            `https://${API_URL}/lol/summoner/v4/summoners/by-name/${textInput}?api_key=${
+              import.meta.env.VITE_API_KEY
+            }`
           );
           const summonerJson = await summonerResponse.json();
           setSummonerData(summonerJson);
@@ -75,13 +79,17 @@ export default function App() {
           // iconURL = `http://ddragon.leagueoflegends.com/cdn/14.3.1/img/profileicon/${iconID}.png`;
 
           const leagueResponse = await fetch(
-            `https://${API_URL}/lol/league/v4/entries/by-summoner/${summonerJson.id}?api_key=${import.meta.env.VITE_API_KEY}`
+            `https://${API_URL}/lol/league/v4/entries/by-summoner/${
+              summonerJson.id
+            }?api_key=${import.meta.env.VITE_API_KEY}`
           );
           const leagueJson = await leagueResponse.json();
           setLeagueData(leagueJson);
           console.log(leagueData);
         } catch (error) {
           console.error(error);
+        } finally {
+          setSpinner(false);
         }
       };
 
@@ -176,8 +184,10 @@ export default function App() {
 
       {renderSummonerData()}
       {renderLeagueRanks()}
-
-
+      <div>
+        {spinner ? <Spinner /> : null}
+        {/* rest of your component */}
+      </div>
 
       {/* <pre>{JSON.stringify(leagueData, null, 2)}</pre> */}
     </>
